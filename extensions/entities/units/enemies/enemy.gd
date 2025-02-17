764910c42285b663d9caf14944f4793a284acd6c
Shawn Hardwick <time4swim@gmail.com>
2025-02-17 02:17:36 -0500

    Add comments and revert some temporary variable debugging statements

--- extensions/entities/units/enemies/enemy.gd
+++ extensions/entities/units/enemies/enemy.gd
@@ -1,7 +1,9 @@
 extends "res://entities/units/enemies/enemy.gd"
 
 func _dmgmeter_get_charm_enemy_effect_behavior_from_hitbox(hitbox: Object) -> CharmEnemyEffectBehavior:
+	# Hitboxes are children of Units and we can reference the parent with 'from'
 	var unit = hitbox.from
+	# We only care to check for effect behavior if it is an enemy
 	if unit is Enemy:
 		var effect_behaviors = unit.effect_behaviors.get_children()
 		for behavior in effect_behaviors:
@@ -11,9 +13,12 @@ func _dmgmeter_get_charm_enemy_effect_behavior_from_hitbox(hitbox: Object) -> Ch
 
 func take_damage(value: int, args: TakeDamageArgs)->Array:
 	var dmg = .take_damage(value, args)
+	# Not everything that does damage has a hitbox
+	# For example, the Lucky character innate ability does damage without a hitbox
 	if args.hitbox:
 		var charm_enemy_effect_behavior = _dmgmeter_get_charm_enemy_effect_behavior_from_hitbox(args.hitbox)
-		if charm_enemy_effect_behavior and charm_enemy_effect_behavior.charmed:
+		# Use get() because above function could return Nil
+		if charm_enemy_effect_behavior.get("charmed"):
 			# Damage is structured as a 3 element array. 
 			# First element is full damage, second element is damage after overkill, third element is boolean for dodge
 			RunData.add_tracked_value(charm_enemy_effect_behavior.charmed_by_player_index, "character_romantic", dmg[1])
--- extensions/singletons/run_data.gd
+++ extensions/singletons/run_data.gd
@@ -2,5 +2,6 @@ extends "res://singletons/run_data.gd"
 
 func init_tracked_effects() -> Dictionary:
 	var tracked_effects = .init_tracked_effects()
+	# All the other items and characters are tracked here so lets add the romantic character
 	tracked_effects["character_romantic"] = 0
 	return tracked_effects
