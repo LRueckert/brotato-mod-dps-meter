378c277133e2ecc48194829f5a0572ee666fa09d
Shawn Hardwick <time4swim@gmail.com>
2025-02-26 23:00:24 -0500

    Fix issue where numbers weren't formatted consistently; for example 12300 was formatted as 12.3K instead of 12.30K

--- extensions/singletons/utils.gd
+++ extensions/singletons/utils.gd
@@ -1,11 +1,15 @@
 extends "res://singletons/utils.gd"	
 
-func format_number(n: float) -> String:
-	if n >= 1_000_000_000:
-		return str(round(n / 1_000_000_000.0 * 100) / 100) + "B"
-	elif n >= 1_000_000:
-		return str(round(n / 1_000_000.0 * 100) / 100) + "M"
-	elif n >= 1_000:
-		return str(round(n / 1_000.0 * 100) / 100) + "K"
-	else:
-		return str(n)
+func format_number(value: int) -> String:
+	var suffixes := ["", "K", "M", "B", "T"]
+	var num := float(value)
+	var index := 0
+	
+	while num >= 1000.0 and index < suffixes.size() - 1:
+		num /= 1000.0
+		index += 1
+	
+	if index == 0:
+		return str(value)
+	
+	return "%.2f%s" % [num, suffixes[index]]
