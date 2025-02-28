0a92f6fe31c946382efc1a19a7c35b17b6f85313
Shawn Hardwick <time4swim@gmail.com>
2025-02-27 21:39:14 -0500

    Fix DLC detection for normal game runtime, previously implementation only accounted for decompiled games running in the editor

--- mod_main.gd
+++ mod_main.gd
@@ -8,22 +8,26 @@ var extensions_dir_path := ""
 var translations_dir_path := ""
 
 func _init() -> void:
+	ModLoaderLog.info("Init!", LRUECKERT_DMGMETER_LOG_NAME)
 	mod_dir_path = ModLoaderMod.get_unpacked_dir().plus_file(LRUECKERT_DMGMETER_DIR)
 	install_script_extensions()
 	add_translations()
 
 
-func install_script_extensions() -> void:
+func install_script_extensions() -> void:	
 	extensions_dir_path = mod_dir_path.plus_file("extensions")
 	ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("ui/hud/dmg_meter.gd"))
 	ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("ui/hud/dmg_meter_positioning.gd"))
 	ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("singletons/run_data.gd"))
 	ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("singletons/utils.gd"))
 	ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("global/dlc_data.gd"))
-	var dlc_list = _get_dlc()
-	if "abyssal_terrors" in dlc_list:
+	# Need to load specific scripts depending on whether certain DLC is found
+	if _get_abyssal_terrors_dlc():
+		ModLoaderLog.info("Configuring Abyssal Terrors-specific scripts", LRUECKERT_DMGMETER_LOG_NAME)
 		ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("entities/units/enemies/enemy.gd"))
 		ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("dlcs/dlc_1/effect_behaviors/enemy/charm_enemy_effect_behavior.gd"))
+	else:
+		ModLoaderLog.info("No DLC found", LRUECKERT_DMGMETER_LOG_NAME)
 	
 
 func add_translations() -> void:
@@ -45,7 +49,7 @@ func add_translations() -> void:
 
 
 func _ready() -> void:
-	ModLoaderLog.info("Ready!", LRUECKERT_DMGMETER_LOG_NAME)
+	ModLoaderLog.info("Ready", LRUECKERT_DMGMETER_LOG_NAME)
 	var mainSzene = load("res://main.tscn").instance()
 	for index in 4:
 		var player_index = str(index + 1)
@@ -57,35 +61,65 @@ func _ready() -> void:
 	var statsContainerScene = load("res://ui/menus/shop/stats_container.tscn").instance()
 	ModLoaderMod.append_node_in_scene(statsContainerScene, "TotalDamageContainer", "MarginContainer/VBoxContainer2/SecondaryStats", "res://mods-unpacked/lrueckert-DmgMeter/ui/menus/shop/last_wave_damage_container.tscn")
 	ModLoaderMod.save_scene(statsContainerScene, "res://ui/menus/shop/stats_container.tscn")
-	ModLoaderLog.info("Done!", LRUECKERT_DMGMETER_LOG_NAME)
+	ModLoaderLog.info("Done", LRUECKERT_DMGMETER_LOG_NAME)
 
-func _get_dlc():
-	var dlc_list: = []
-	var dir = Directory.new()
-	var dir_path = "res://dlcs/"
 
-	if not dir.dir_exists(dir_path):
-		return dlc_list
-
-	dir.open(dir_path)
-	dir.list_dir_begin(true)
-
-	var dlc_dirs: Array = []
-
-	var dir_name = dir.get_next()
-	while dir_name != "":
-		if dir.current_is_dir():
-			dlc_dirs.push_back(dir_path + dir_name)
-		dir_name = dir.get_next()
-
-	for path in dlc_dirs:
-		dir.open(path)
+# The method for determining what DLC is available depending on whether we are in context
+# of the editor/decompiled game or normal game runtime
+# Because the script extensions are installed prior game runtime, we have to initialize our
+# own DLC detection
+func _get_abyssal_terrors_dlc() -> bool:	
+	if OS.has_feature("editor"):
+		ModLoaderLog.info("Checking for DLC in editor mode", LRUECKERT_DMGMETER_LOG_NAME)
+		var dir = Directory.new()
+		var dir_path = "res://dlcs/"
+		ModLoaderLog.info("Checking " + dir_path, LRUECKERT_DMGMETER_LOG_NAME)
+		if not dir.dir_exists(dir_path):
+			ModLoaderLog.info(dir_path + " does not exist", LRUECKERT_DMGMETER_LOG_NAME)
+			return false
+	
+		dir.open(dir_path)
 		dir.list_dir_begin(true)
-		var file_name = dir.get_next()
-		while file_name != "":
-			if file_name == "dlc_data.tres":
-				var dlc_data: = load(path + "/" + file_name) as DLCData
-				dlc_list.push_back(dlc_data.my_id)
-			file_name = dir.get_next()
+	
+		var dlc_dirs: Array = []
+	
+		var dir_name = dir.get_next()
+		while dir_name != "":
+			if dir.current_is_dir():
+				dlc_dirs.push_back(dir_path + dir_name)
+			dir_name = dir.get_next()
+	
+		for path in dlc_dirs:
+			dir.open(path)
+			dir.list_dir_begin(true)
+			var file_name = dir.get_next()
+			ModLoaderLog.info("Found DLC: " + file_name, LRUECKERT_DMGMETER_LOG_NAME)
+			while file_name != "":
+				if file_name == "dlc_data.tres":
+					var dlc_data: = load(path + "/" + file_name) as DLCData
+					if dlc_data.my_id == "abyssal_terrors":
+						return true
+				file_name = dir.get_next()
+		return false
+	else:
+		ModLoaderLog.info("Checking for DLC in normal runtime mode", LRUECKERT_DMGMETER_LOG_NAME)
+		return _load_dlc_pcks()
 
-	return dlc_list
+# Taken from ProgressData.load_dlc_packs
+func _load_dlc_pcks() -> bool :
+	var file = File.new()
+	var dlc_path: String = _get_game_dir() + "/" + "BrotatoAbyssalTerrors.pck"
+	ModLoaderLog.info("Searching Abyssal Terrors DLC at " + dlc_path, LRUECKERT_DMGMETER_LOG_NAME)
+	return file.file_exists(dlc_path)
+
+# Taken from Utils.get_game_dir
+func _get_game_dir() -> String:
+	var game_install_directory: = OS.get_executable_path().get_base_dir()
+
+	if OS.get_name() == "OSX":
+		game_install_directory = game_install_directory.get_base_dir().get_base_dir()
+
+	if OS.has_feature("editor"):
+		game_install_directory = "res://"
+
+	return game_install_directory
